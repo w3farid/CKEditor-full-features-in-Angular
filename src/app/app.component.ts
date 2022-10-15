8812f60621dff638c2212cb607b84f32063b80e1
Farid Ahmed <w3farid@gmail.com>
2022-10-15 10:42:27 +0600

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Renderer2, Inject, Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ck-demo';
+
+  constructor(
+    private renderer2: Renderer2,
+    @Inject(DOCUMENT) private _document:any
+  ) {}
+
+  ngOnInit() {
+    this.addScriptInComponent('assets/js/ckeditor-demo.js');
+ }
+
+ addScriptInComponent(url: string){
+  const s = this.renderer2.createElement('script');
+  s.type = 'text/javascript';
+  s.src = url;
+  s.text = ``;
+  this.renderer2.appendChild(this._document.body, s);
+ }
 }
